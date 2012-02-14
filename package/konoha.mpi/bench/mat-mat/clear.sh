7e0d47631987616ccd7acdbe068eaa559289cbe5
utrhira <utr.hira@gmail.com>
2012-02-14 23:19:06 +0900

    fix clear.sh

--- package/konoha.mpi/bench/mat-mat/clear.sh
+++ package/konoha.mpi/bench/mat-mat/clear.sh
@@ -3,10 +3,17 @@
 now=`date "+%m%d_%H%M%S"`
 tmp=${now}
 mkdir $tmp
-for log in `find . -name "mm_*"`; do
-    if [ `wc -l $log` = 0 ]; then
+flag=0
+for log in `find . -maxdepth 1 -mindepth 1 -name "mm_*"`; do
+    ln=(`wc -l $log`)
+    if [ $ln = 0 ]; then
         rm $log
     else
+        flag=1
         mv $log ${tmp}/.
     fi
 done
+
+if [ $flag = 0 ]; then
+    rmdir $tmp
+fi
